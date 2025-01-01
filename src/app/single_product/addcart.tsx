2ac40a8a792d6a0e42515ed28b51538d367b401d
hammad shah <164568796+hammadshah07@users.noreply.github.com>
2025-01-01 19:22:30 +0500

    Update addcart.tsx

--- src/app/single_product/addcart.tsx
+++ src/app/single_product/addcart.tsx
@@ -23,7 +23,7 @@
 //         <SheetHeader>
 //           <SheetTitle>Edit Profile</SheetTitle>
 //           <SheetDescription>
-//             Make changes to your profile here. Click save when you're done.
+//             Make changes to your profile here. Click save when you&apos;re done.
 //           </SheetDescription>
 //         </SheetHeader>
 //         <div className="grid gap-4 py-4">
